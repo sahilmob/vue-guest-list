426a9fe265249a1d3e1e88c9c41d20804b70892b
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-10 17:15:55 +0300

    Init Vue instance, add data object and use interpolation to output event date, title and description from the data object

---
+++ app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        event:{
+            date: 'August 14th - 16th',
+            title: 'Summer Festival!',
+            description: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+        }
+    }
+})
