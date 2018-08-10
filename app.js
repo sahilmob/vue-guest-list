1f69c2ba383bcaad0538fa5b5d03679929840cb3
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-10 17:22:58 +0300

    Setup two way bindind for the name input

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ new Vue({
             date: 'August 14th - 16th',
             title: 'Summer Festival!',
             description: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
-        }
+        },
+        newNameText: ''
     }
 })
