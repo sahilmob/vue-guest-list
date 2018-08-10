d0049f45e6987e2afa33480fb280b9a9f15d7a6e
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-08-10 17:43:25 +0300

    Add conditional rendering to display 'No names added yet' if no names added, or display guests name if there is names added

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ new Vue({
     },
     methods: {
         formSubmit(){
-            this.guestName.push(this.newNameText)
-            this.newNameText = ''
+            if (this.newNameText.length > 0){
+                this.guestName.push(this.newNameText)
+                this.newNameText = ''
+            }
         }
     }
 })
